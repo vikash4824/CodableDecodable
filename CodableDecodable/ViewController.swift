fcf60b9393e58fbcbb43bb655ed93a2b89aef7c9
vikash4824 <vamsivikash24@gmail.com>
2021-03-05 09:38:56 +0530

    Initial Commit

---
+++ CodableDecodable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodableDecodable
+//
+//  Created by mamidisetty Vikash on 05/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
